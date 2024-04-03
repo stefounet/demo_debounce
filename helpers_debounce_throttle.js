f7eb6d936c517454277773ea349d8a39ad227b1f
stefounet <s@fasterize.com>
2024-04-03 14:17:22 +0000

    first commit

---
+++ helpers_debounce_throttle.js
@@ -0,0 +1,91 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+/**
+ * Created by thephpjo on 21.04.14.
+ */
+
+
+var helpers = {
+
+    /**
+     * debouncing, executes the function if there was no new event in $wait milliseconds
+     * @param func
+     * @param wait
+     * @param scope
+     * @returns {Function}
+     */
+    debounce: function (func, wait, scope) {
+        var timeout;
+        return function () {
+            var context = scope || this, args = arguments;
+            var later = function () {
+                timeout = null;
+                func.apply(context, args);
+            };
+            clearTimeout(timeout);
+            timeout = setTimeout(later, wait);
+        };
+    },
+
+    /**
+     * in case of a "storm of events", this executes once every $threshold
+     * @param fn
+     * @param threshhold
+     * @param scope
+     * @returns {Function}
+     */
+    throttle: function (fn, threshhold, scope) {
+        threshhold || (threshhold = 250);
+        var last,
+            deferTimer;
+        return function () {
+            var context = scope || this;
+
+            var now = +new Date,
+                args = arguments;
+            if (last && now < last + threshhold) {
+                // hold on to it
+                clearTimeout(deferTimer);
+                deferTimer = setTimeout(function () {
+                    last = now;
+                    fn.apply(context, args);
+                }, threshhold);
+            } else {
+                last = now;
+                fn.apply(context, args);
+            }
+        };
+    }
+}
+
+}
+/*
+     FILE ARCHIVED ON 09:23:27 Jan 17, 2022 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 13:22:52 Apr 03, 2024.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.549
+  exclusion.robots: 0.066
+  exclusion.robots.policy: 0.058
+  cdx.remote: 0.051
+  esindex: 0.01
+  LoadShardBlock: 47.629 (3)
+  PetaboxLoader3.datanode: 400.69 (4)
+  load_resource: 555.731
+  PetaboxLoader3.resolve: 190.894
+*/
